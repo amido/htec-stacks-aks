57429af263e328e063d1266ce93b045080502125
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2022-04-09 12:35:42 +0100

    Initial checkin of infrastrtcure for AKS cluster
    Signed by Russell Seymour <russell.seymour@turtlesystems.co.uk>

---
+++ deploy/azure/infra/stacks-aks/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "Stacks-Ancillary-Resources"
+    storage_account_name = "htecstackstfstate"
+    container_name       = "tfstate"
+    key                  = "core-sharedservices"    
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
